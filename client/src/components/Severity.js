ececd90de0d9a0617675cc0434ad66145551c4de
Shawn Monel <smonel@mit.edu>
2020-09-20 06:12:31 -0400

    Further Changes

--- client/src/components/Severity.js
+++ client/src/components/Severity.js
@@ -1,25 +1,7 @@
 import React from 'react';
 
 class Severity extends React.Component{
-    componentDidMount(){
-        var dial = (".dial .inner");
-        var gauge_value = (".gauge .value");
 
-            function rotateDial(){
-                var deg = 0;
-                var value = Math.round(83.12);
-                deg = (value * 177.5) / 100;
-                gauge_value.html(value);
-
-                dial.css({'transform': 'rotate('+deg+'deg)'});
-                dial.css({'-ms-transform': 'rotate('+deg+'deg)'});
-                dial.css({'-moz-transform': 'rotate('+deg+'deg)'});
-                dial.css({'-o-transform': 'rotate('+deg+'deg)'});
-                dial.css({'-webkit-transform': 'rotate('+deg+'deg)'});
-            }
-
-            setInterval(rotateDial, 2000);
-    }
     render() {
 
         return(<>
--- client/src/components/Story.js
+++ client/src/components/Story.js
@@ -1,39 +1,31 @@
 import React from 'react';
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = deg2rad(lon2-lon1); 
-    var a = 
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-      Math.sin(dLon/2) * Math.sin(dLon/2)
-      ; 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c; // Distance in km
-    var meters = d / 1000
-    return meters;
-  }
-  
-  function deg2rad(deg) {
-    return deg * (Math.PI/180)
-  }
+let api_key = "315ff1e75f172edd76f71aa5afccbc16";
 
-class Distance extends React.Component {
+class Story extends React.Component {
     render() {
-      let lat1 ;
-      let lon1 ;
-      let lat2 ;
-      let lon2 ;
-      distance_traveled = getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2)
+      let lat1 = this.props.startPoint.lat;
+      let lon1 = this.props.startPoint.lng;
+      let lat2 = this.props.endPoint.lat;
+      let lon2 = this.props.endPoint.lng;
+      let distance_traveled = getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2);
+      let beginning = new Date(this.props.startTime);
+      beginning = beginning.toString();
+      let date = new Date(this.props.endTime - this.props.startTime);
+      let duration = date.toLocaleTimeString();
       return(
           <div class="card">
               <div class = "card-body">
-                <h2>Distance of the crash</h2>
-                <p>How far did the vehicle traveled?</p>
+                <h1 class="display-4">Distance and Time</h1>
+                <h3>When did the crash occur?</h3>
+                <p>{beginning}</p>
+                <h3>How far did the vehicle travel during the crash?</h3>
                 <p>{distance_traveled}</p>
-                <p>Where is the vehicle now?</p>
+                <h3>What was the duration of the crash?</h3>
+                <p>{duration}</p>
               </div>
           </div>
       );
   }
 }
+
+export default Story;
