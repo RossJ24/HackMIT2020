6b3e4ee56d1ab727ef3a35cbb741ea71158ac287
Ross Johnson <RossJ24@users.noreply.github.com>
2020-09-20 09:35:26 -0400

    Edit gitignore to show build folder

---
+++ client/build/precache-manifest.ed33bec0a6e259b2124ebb2c08406326.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0e39fb5d27ed7598c52c986a8861c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbff62aefb721c05ef8f",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "621e313c9b0f9fd053fe",
+    "url": "/static/css/main.7180161a.chunk.css"
+  },
+  {
+    "revision": "cbff62aefb721c05ef8f",
+    "url": "/static/js/2.474365ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.474365ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621e313c9b0f9fd053fe",
+    "url": "/static/js/main.4a4b6ac9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
